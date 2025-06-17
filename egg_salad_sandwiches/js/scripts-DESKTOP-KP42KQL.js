8a0da80100d6e43323c0a49ee4ec0da619f1fccf
Mackenzie Nelson <208896789+ReinadeDragona@users.noreply.github.com>
2025-06-17 09:28:57 -0500

    Adding More Recipes

---
+++ egg_salad_sandwiches/js/scripts-DESKTOP-KP42KQL.js
@@ -0,0 +1,33 @@
+
+
+<button class="portion1" type="button" onclick="changecontent">
+	1x
+</button>
+
+<button class="portion2" type="button" onclick="changecontent">
+	1x
+</button>
+
+<button class="portion3" type="button" onclick="changecontent">
+	1x
+</button>
+
+<div class="pagename">
+    <h1><u>Recipe: Egg Salad Sandwiches <span class="smalltext">(simple)</span></u></h1>
+
+			<p class="author">Written By: Mackenzie Nelson</p>
+
+			<img alt="" class="image"
+				src="https://www.allrecipes.com/thmb/UHtxAb9VrJgbtNxsxQfsdCrsXB0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/104804-curried-egg-sandwiches-ddmfs-3x4-c75f6bc7c4f24588a2bf6b59e33b2e37.jpg">
+
+			<ul id="overview-summary">
+				<li>Ingredients: 3</li>
+				<li>Steps: 13</li>
+				<li>Total Time: 1 Hour</li>
+			</ul>
+</div>
+
+/*<details class="portions">
+	<summary>Portions</summary>
+		10 Sandwiches (will need to make another batch to do the portion math)
+</details>*/
---
+++ egg_salad_sandwiches/js/scripts.js
@@ -0,0 +1,37 @@
+const buttons = document.getElementById("buttons");
+
+buttons.addEventListener('click'myfunction(steps2){
+	document.getElementsByClassName('portion2').innertext = 'steps2'
+
+
+
+
+
+
+
+
+/*<button class="portion1" type="button">
+	1x:10 Sandwiches
+</button>
+<button class="portion2" type="button">
+		2x:20 Sandwiches
+</button>
+	
+<button class="portion3" type="button">
+		3x:0 Sandwiches
+</button>		
+
+document.getElementsByClassName(buttons)
+
+function myfunction() {
+	if(class="portion1")
+		console.log(steps1);
+
+	if (class="portion2") {
+		console.log(steps2);
+	{"}"}
+
+	if (class="portion3") {
+		console.log(steps3);
+}*/
+
